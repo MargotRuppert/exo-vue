4227b10015bb1603f063a74dd9ae790f00b0a4bb
MargotRuppert <margot.ruppert@gmail.com>
2025-09-09 12:00:04 +0200

    premier ajout exo vue et exo nuxt

---
+++ cours-vue/vue-projet/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<p>{{ number }}</p>
+<p>{{ string }}</p>
+<p>{{ tab }}</p>
+<p>{{ boolean }}</p>
+<p>{{ fun() }}</p>
+</template>
+
+<script setup>
+let number = 1;
+let string = "salut";
+let tab = [1, 2, 3, 4];
+let boolean = false;
+function fun(){
+  return 'salut';
+}
+</script>
+
+<style scoped>
+
+</style>
