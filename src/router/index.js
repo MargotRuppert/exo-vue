5999c390c4b52227639c3af048a22f44a5596e2a
MargotRuppert <margot.ruppert@gmail.com>
2025-09-09 15:34:44 +0200

    oops correction

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory} from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            alias: '/home',
+            name: 'home',
+            component: () => import('../views/Home.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/About.vue')
+        }
+    ]
+});
+
+export default router;
