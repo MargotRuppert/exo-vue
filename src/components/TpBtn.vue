a3556e42412de91f9b1443cad600581c3e542a3e
MargotRuppert <margot.ruppert@gmail.com>
2025-09-10 11:03:20 +0200

    exo event binding

---
+++ src/components/TpBtn.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="btn">
+        <button @click.right="btnDown" class="btn btn-square btn-error">-</button>
+        <p>Le nombre : {{ number }}</p>
+        <button @click.middle="btnUp" class="btn btn-square btn-success">+</button>
+
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let number = ref(0);
+console.log(number);
+
+function btnUp(){
+    number.value += 5;
+}
+
+function btnDown(){
+    number.value -= 11;
+}
+
+</script>
+
+<style>
+#btn {
+    margin-top : 50px;
+    display: flex;
+    flex-direction: row;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
 <TpData></TpData>
+<TpBtn></TpBtn>
 </template>
 
 <script setup lang='js'>
 import TpData from '../components/TpData.vue';
+import TpBtn from '../components/TpBtn.vue';
 
 </script>
 
