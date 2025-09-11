ac6fae904977293a0028999a51e35befb5fd9c16
MargotRuppert <margot.ruppert@gmail.com>
2025-09-11 16:50:11 +0200

    ajout v-of

--- src/components/VifExo.vue
+++ src/components/VifExo.vue
@@ -3,9 +3,9 @@
         <div class="card shadow-lg p-6 bg-base-100">
             <h2 class="text-2xl font-bold mb-4">WatchList 🎬 (Films à voir) - Mode : Composition</h2>
 
-            <input v-model="ajout" type="text" placeholder="Entrez le nom d'un film" class="input input-bordered w-full mb-3" />
+            <input v-model="film" type="text" placeholder="Entrez le nom d'un film" class="input input-bordered w-full mb-3" />
 
-            <button @click="tabFilm.push(ajout)" class="btn btn-primary mb-4">
+            <button @click="tabFilm.push(film)" class="btn btn-primary mb-4">
                 Ajouter à votre Liste
             </button>
 
@@ -15,8 +15,8 @@
                 <h3 class="text-xl font-semibold mb-2">Votre film ajouté : </h3>
             </div>
 
-            <p class="text-gray-500" v-if="tabFilm[0]">
-                {{ tabFilm[0] }}
+            <p class="text-gray-500" v-if="tabFilm.length" v-for="(film, index) in tabFilm" @click="tabFilm.splice(index, 1)">
+                 {{`${index} ${film}` }}
             </p>
 
             <p class="text-gray-500" v-else>
@@ -30,6 +30,6 @@
 import {ref} from 'vue'
 const tabFilm = ref([]);
 
-const ajout = ref("");
+const film = ref("");
 
 </script>
