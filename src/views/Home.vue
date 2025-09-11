0719cb51010abd991ce7f008e17517af58d2ccb4
MargotRuppert <margot.ruppert@gmail.com>
2025-09-11 15:52:34 +0200

    exo v-if

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,8 @@
 <TpEvent></TpEvent> -->
 <!-- <TpWatcher></TpWatcher> -->
  <!-- <DynamicStyle></DynamicStyle> -->
-<StyleDynamic></StyleDynamic>
+<!-- <StyleDynamic></StyleDynamic> -->
+<VifExo></VifExo>
 </template>
 
 <script setup lang='js'>
@@ -14,6 +15,7 @@ import TpEvent from '../components/TpEvent.vue';
 import TpWatcher from '../components/TpWatcher.vue';
 import DynamicStyle from '../components/DynamicStyle.vue';
 import StyleDynamic from '../components/StyleDynamic.vue';
+import VifExo from '../components/VifExo.vue';
 
 </script>
 
